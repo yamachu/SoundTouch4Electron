423b6fb23f92d96a278be97e36e5cda5fa02b721
Yusuke Yamada <yusuke.yamada.t1@dc.tohoku.ac.jp>
2017-02-05 18:06:51 +0900

    Update check script

--- check.js
+++ check.js
@@ -1,5 +1,6 @@
 // Overview of edge.js: http://tjanczuk.github.com/edge
-
+// import electron-edge: require node module 50
+console.dir(process.versions);
 var edge = require('electron-edge');
 
 var hello = edge.func('async (input) => { return ".NET welcomes " + input.ToString(); }');
@@ -7,4 +8,6 @@ var hello = edge.func('async (input) => { return ".NET welcomes " + input.ToStri
 hello('Node.js, electron-edge enabled!', function (error, result) {
 	if (error) throw error;
 	console.log(result);
+
+	process.exit();
 });
